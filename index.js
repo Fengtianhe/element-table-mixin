4979473da224ccda13654b10f1f71ada746d6dd5
fengtianhe <545704061@qq.com>
2020-11-28 20:44:36 +0800

    注册组件

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-export default {
+import TableContainer from './src/TableContainer'
+import TableMixin from './src/TableMixin'
+import TableMixinConfig from './src/TableMixinConfig'
+import TableMixinObjectHelper from './src/ObjectHelper'
 
+export default {
+  TableContainer,
+  TableMixin,
+  TableMixinConfig,
+  TableMixinObjectHelper
 }
